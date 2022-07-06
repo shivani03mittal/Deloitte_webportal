62ed8f46a87b1240836f63f395a2496e0878ee89
shivani03mittal <shivani03mittal@gmail.com>
2022-07-06 17:08:56 +0530

    Add project files.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+using WebApp.Data;
+using WebApp.Models;
+using File = WebApp.Models.File;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly FilesDbContext _files;
+
+        public HomeController(FilesDbContext files, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _files = files;
+        }
+
+        public IActionResult Index()
+        {
+            // Filter the files for a the logged in user
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            IEnumerable<File> FileList = _files.File.Where(x => x.UserId == userId);
+            return View(FileList);
+        }
+
+        public IActionResult FileData(int file)
+        {
+            IEnumerable<File> CurrentFile = _files.File.Where(x => x.Id == file);
+            return Ok(CurrentFile);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication/Models/File.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public DateTime Created { get; set; }
+        public int Size { get; set; }
+        public string UserId { get; set; }
+    }
+}
